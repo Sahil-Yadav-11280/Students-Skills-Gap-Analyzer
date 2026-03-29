d2cbef5cbf3c878b56d6fb785ba0e2bd8af0c0a1
victo-silvers <silversvicto@gmail.com>
2026-03-30 04:14:50 +0530

    Implement DashboardController with CORS support

--- Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/controller/DashboardController.java
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/controller/DashboardController.java
@@ -8,7 +8,24 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.lang.model.util.Elements;
 
- // Allows this api to be called with CORS errors
+@RestController
+@RequestMapping("/api/dashboard")
+@CrossOrigin(origins = "*") // Allows this api to be called with CORS errors
 public class DashboardController {
 
+    private final DashboardService dashboardService;
+    public DashboardController(DashboardService dashboardService){
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<DashboardDataResponse> getDashboardForStudent(@PathVariable Long studentId){
+        try{
+            DashboardDataResponse response = dashboardService.getStudentDashboard(studentId);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+
+    }
 }
