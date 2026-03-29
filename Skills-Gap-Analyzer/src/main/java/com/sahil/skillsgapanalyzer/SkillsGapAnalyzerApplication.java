cb7d64d2293cb70ebd521545040cd30bbfc02675
victo-silvers <silversvicto@gmail.com>
2026-03-29 12:20:50 +0530

    Initiated SpringBoot backend

---
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/SkillsGapAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.sahil.skillsgapanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsGapAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillsGapAnalyzerApplication.class, args);
+    }
+
+}
---
+++ Skills-Gap-Analyzer/src/test/java/com/sahil/skillsgapanalyzer/SkillsGapAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.skillsgapanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsGapAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
