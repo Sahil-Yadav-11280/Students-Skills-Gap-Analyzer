bdcdb5bd2583c314795d16add7ca3ac6da35c96d
victo-silvers <silversvicto@gmail.com>
2026-03-30 01:37:38 +0530

    defined Response dto

--- Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/MLPredictionResponse.java
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/MLPredictionResponse.java
@@ -2,5 +2,8 @@ package com.sahil.skillsgapanalyzer.dto;
 
 // What Flask sends back to SpringBoot
 public class MLPredictionResponse {
-
+    public String status;
+    public Double predicted_probability;
+    public Double mastery_score_percentage;
+    public Double model_confidence;
 }
