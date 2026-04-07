735ea284f95f81d234c168494b4ae600bb56daa4
victo-silvers <silversvicto@gmail.com>
2026-04-07 06:36:01 +0530

    student addition dto

---
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/AddStudentRequest.java
@@ -0,0 +1,14 @@
+package com.sahil.skillsgapanalyzer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddStudentRequest {
+    private String name;
+}
