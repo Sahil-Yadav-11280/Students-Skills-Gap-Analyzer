8e5686934fc7c0d160fe2b6ec15dcfef478c3554
victo-silvers <silversvicto@gmail.com>
2026-04-07 06:38:06 +0530

    Setting up client for new model

--- Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/integration/MlPredictionClient.java
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/integration/MlPredictionClient.java
@@ -2,9 +2,13 @@ package com.sahil.skillsgapanalyzer.integration;
 
 import com.sahil.skillsgapanalyzer.dto.MLPredictionResponse;
 import com.sahil.skillsgapanalyzer.dto.MlPredictionRequest;
+import com.sahil.skillsgapanalyzer.dto.SkillPredictionDto;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class MlPredictionClient {
@@ -18,19 +22,18 @@ public class MlPredictionClient {
     public MLPredictionResponse getPrediction(MlPredictionRequest requestPayload){
         try{
             return restClient.post()
-                    .uri("/predict")
+                    .uri("http://localhost:5000/predict")
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(requestPayload)
                     .retrieve()
                     .body(MLPredictionResponse.class);
-        } catch (Exception e){
-            System.err.println("Error calling Flask API: "+e.getMessage());
+        }
+        catch (Exception e){
+            System.err.println("Error calling Flask API: " + e.getMessage());
 
             MLPredictionResponse fallback = new MLPredictionResponse();
-            fallback.status = "error";
-            fallback.predicted_probability = 0.0;
-            fallback.mastery_score_percentage = 0.0;
-            fallback.model_confidence = 0.0;
+            fallback.api_status = "error";
+            fallback.skills_data = new HashMap<>();   // ✅ FIX
 
             return fallback;
         }
