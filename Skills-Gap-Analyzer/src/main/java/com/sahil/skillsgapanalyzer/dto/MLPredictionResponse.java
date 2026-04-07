03d2e7071ca97d7eccff516099d35a2e4c97bb9e
victo-silvers <silversvicto@gmail.com>
2026-04-07 06:38:24 +0530

    prediction response dto for new model

--- Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/MLPredictionResponse.java
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/MLPredictionResponse.java
@@ -1,9 +1,15 @@
 package com.sahil.skillsgapanalyzer.dto;
 
+import java.util.Map;
+
 // What Flask sends back to SpringBoot
 public class MLPredictionResponse {
-    public String status;
-    public Double predicted_probability;
-    public Double mastery_score_percentage;
-    public Double model_confidence;
+    public String api_status;
+    public Map<String , SkillPredictionData> skills_data;
+
+    public static class SkillPredictionData {
+        public Double predicted_probability;
+        public Double mastery_score_percentage;
+        public Double model_confidence;
+    }
 }
