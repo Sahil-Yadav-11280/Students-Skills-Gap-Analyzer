9c3b5e59b83b65b1b56a627d98fa5c9d613d0abd
victo-silvers <silversvicto@gmail.com>
2026-04-07 06:46:29 +0530

    completed dto for edit of attempt history

--- Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/StudentAttemptDto.java
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/StudentAttemptDto.java
@@ -1,4 +1,17 @@
 package com.sahil.skillsgapanalyzer.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentAttemptDto {
+    private Long id;
+    private String skill;
+    private Integer correct;
+    private Long actionNum;
+    private Integer hintCount;
+    private Double timeTaken;
 }
